02a8e6d0dcaf82bf06ce5d071db156da2d36c20e
crouvel <55972660+crouvel@users.noreply.github.com>
2022-11-19 17:29:55 +0100

    first commit

---
+++ src/main/java/com/example/offres_microservice/model/Offre.java
@@ -0,0 +1,13 @@
+package com.example.offres_microservice.model;
+
+import javax.persistence.*;
+/*import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;*/
+
+@Entity
+@Table(	name = "offres")
+public class Offre {
+
+
+}
---
+++ src/test/java/com/example/offres_microservice/OffresMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.offres_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffresMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
